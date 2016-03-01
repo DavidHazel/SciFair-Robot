db8e890fe88156f759bd158918229ab9d3e5be72
Daniel Hug <daniel@hugwebdesign.com>
2016-02-29 17:47:58 -0800

    add config form validation

--- view.js
+++ view.js
@@ -1,43 +1,98 @@
 
 
-// get matrix size from input
-var getMatrixSize = (function() {
-	// get input
-	var matrixSizeInput = document.getElementById('MatrixSize');
 
-	// update matrix size whenever input changes
-	matrixSizeInput.addEventListener('input', function setMatrixSize() {
+
+/*
+	don't do anything upon form submit (the form is for input validation)
+*/
+
+var configForm = document.getElementById('MatrixSize');
+configForm.addEventListener('submit', function handleConfigFormSubmit(event) {
+	event.preventDefault();
+});
+
+
+
+
+/*
+	"matrix size" input
+*/
+
+var matrixSizeInput = document.getElementById('MatrixSize');
+
+// update matrix size whenever input changes
+matrixSizeInput.addEventListener('input', function setMatrixSize() {
+	if (this.validity.valid) {
 		// change matrix size
 		m.set('size', getMatrixSize());
-	});
+	} else {
+		// show error message if invalid
+		this.form.reportValidity();
+	}
+});
 
-	return function() {
-		// get the value, convert to a base 10 number
-		return parseInt(matrixSizeInput.value, 10);
-	};
-})();
-
-
-// get fatty acid count from input
-var getFattyAcidCount = (function() {
-	// get input
-	var fattyAcidCountInput = document.getElementById('FattyAcidCount');
-
-	// update fatty acid count whenever input changes
-	fattyAcidCountInput.addEventListener('input', updateFattyAcidCount);
-
-	return function() {
-		// get the value, convert to a base 10 number
-		return parseInt(fattyAcidCountInput.value, 10);
-	};
-})();
+function getMatrixSize() {
+	// get the value, convert to a base 10 number
+	return parseInt(matrixSizeInput.value, 10);
+}
 
 
 
 
-// m is a 2d array filled with zeros
-var initialSize = getMatrixSize();
-var m = new Matrix(initialSize);
+/*
+	"fatty acid count" input
+*/
+
+var fattyAcidCountInput = document.getElementById('FattyAcidCount');
+
+// update fatty acid count whenever input changes
+fattyAcidCountInput.addEventListener('input', function updateFattyAcidCount() {
+	if (this.validity.valid) {
+		// update fatty acid count
+		m.set('fattyAcidCount', getFattyAcidCount());
+	} else {
+		// show error message if invalid
+		this.form.reportValidity();
+	}
+});
+
+// handy function for getting the input's value
+function getFattyAcidCount() {
+	// get the value, convert to a base 10 number
+	return parseInt(fattyAcidCountInput.value, 10);
+}
+
+
+
+
+/*
+	initialize matrix object
+*/
+
+var m = new Matrix({
+	size: getMatrixSize(),
+	fattyAcidCount: getFattyAcidCount()
+});
+
+
+
+
+// keep the max attribute on the "fatty acid count" input updated to be size*size
+dom({
+	el: fattyAcidCountInput,
+	max: m.snoop('size', function(size) {
+		return size*size;
+	})
+});
+
+// keep the min attribute on the "matrix size" input updated
+// to be the square root of fattyAcidCount rounded up
+dom({
+	el: matrixSizeInput,
+	min: m.snoop('fattyAcidCount', function(fattyAcidCount) {
+		return Math.ceil(Math.sqrt(fattyAcidCount));
+	})
+});
 
 
 
@@ -45,10 +100,8 @@ var m = new Matrix(initialSize);
 // set fatty acid count now and whenever input changes
 function updateFattyAcidCount() {
 	// update fatty acid count
-	m.insertRandomFattyAcids(getFattyAcidCount());
+	m.insertRandomFattyAcids();
 }
-
-updateFattyAcidCount();
 
 
 
